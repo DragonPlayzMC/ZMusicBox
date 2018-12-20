4f4edf8078b1afc353a6b91febd0e1aed0558e60
DragonPlayzMC <jeraldpadua@gmail.com>
2018-12-20 10:19:41 +0900

    fixed

--- src/ZMusicBox/ZMusicBox.php
+++ src/ZMusicBox/ZMusicBox.php
@@ -210,10 +210,11 @@ class ZMusicBox extends PluginBase implements Listener{
 						$pk->y = $block->y;
 						$pk->z = $block->z;*/
 						$pk->position = new Vector3($block->x, $block->y, $block->z);
-						$pk->volume = $type;
-						$pk->pitch = $sound;
-						$pk->unknownBool = true;
-						$pk->unknownBool2 = true;
+						//$pk->volume = $type; //old
+						//$pk->pitch = $sound; //old
+						$pk->extraData = $sound; //**new changes**
+						//$pk->unknownBool = true; //old
+						//$pk->unknownBool2 = true; //old 
 						$p->dataPacket($pk);
 					}
 				}
